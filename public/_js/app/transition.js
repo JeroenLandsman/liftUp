a6f9fe05977917da16e3f0c31aacaf2dba1843ea
Sebastiaan Scheers <zwooosh@gmail.com>
2017-10-04 15:08:45 +0200

    slider animatie

--- public/_js/app/transition.js
+++ public/_js/app/transition.js
@@ -1,24 +1,24 @@
-;(function ($) {
-  'use strict';
-  var content  = $('#main').smoothState({
-        // onStart runs as soon as link has been activated
-        onStart : {
+// ;(function ($) {
+//   'use strict';
+//   var content  = $('#main').smoothState({
+//         // onStart runs as soon as link has been activated
+//         onStart : {
           
-          // Set the duration of our animation
-          duration: 250,
+//           // Set the duration of our animation
+//           duration: 250,
           
-          // Alterations to the page
-          render: function () {
+//           // Alterations to the page
+//           render: function () {
 
-            // Quickly toggles a class and restarts css animations
-            content.toggleAnimationClass('is-exiting');
+//             // Quickly toggles a class and restarts css animations
+//             content.toggleAnimationClass('is-exiting');
 
-            // Scroll user to the top
-            $body.animate({ 'scrollTop': 0 });
-          }
-        }
-      }).data('smoothState'); // makes public methods available
-})(jQuery);
+//             // Scroll user to the top
+//             $body.animate({ 'scrollTop': 0 });
+//           }
+//         }
+//       }).data('smoothState'); // makes public methods available
+// })(jQuery);
 
 
 $(document).ready(function() {
@@ -29,16 +29,42 @@ $(document).ready(function() {
         bertWidth = $bert.width(),
         bertHeight = $bert.height(),
         bertPos = $bert.offset(),
-        url = $(this).data("location"),
-        $inner = $(".c-bert__inner");
-    $bertOverlay.css({"height": bertHeight, "width" : bertWidth, "z-index": 9999, "top" : bertPos.top, "left" : bertPos.left});
-    $inner.fadeOut();    
-    $bertOverlay.animate({"height": "100%", "width" : "100%", "top" : 0, "left" : 0},{
+        url = $(this).data("location");
+    $bertOverlay.css({"height": bertHeight, "width" : bertWidth, "z-index": 9999, "top" : bertPos.top, "left" : bertPos.left}); 
+    $bertOverlay.animate({"opacity": 1},{
       duration: 800,
       complete: function(){
-        console.log(url);
-        window.location.href = url;
+        $bertOverlay.animate({"height": "100%", "width" : "100%", "top" : 0, "left" : 0},{
+          duration: 800,
+          complete: function(){
+            window.location.href = url;
+          }
+        });  
       }
-    });    
+    });  
   });  
+
+  //slider transition
+  $("a.c-slider__link").on('click',function(event){
+    var height = $(this).height(),
+        width = $(this).width(),
+        $bertOverlay = $("#bertOverlay"),
+        offset = $(this).offset(),
+        url = $(this).attr("href"),
+        type = $(this).data("type");
+    event.preventDefault();
+    $bertOverlay.addClass(type);
+    $bertOverlay.css({"height": height, "width" : width, "z-index": 9999, "top" : offset.top, "left" : offset.left});
+    $bertOverlay.animate({"opacity": 1},{
+      duration: 800,
+      complete: function(){
+        $bertOverlay.animate({"height": "100%", "width" : "100%", "top" : 0, "left" : 0},{
+          duration: 800,
+          complete: function(){
+            window.location.href = url;
+          }
+        });  
+      }
+    });
+  }); 
 });
